c4f8c0fd8ef8b03ec493a6fd2affc192b66ffc63
Hyunjin LIM <limhyun14@gmail.com>
2026-06-24 16:08:54 +0900

    Feat: 프로젝트 초기 세팅(Express server structure)

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const app = express();
 const PORT = 3001;
 
 // 미들웨어 설정
-app.use(cors());                                                //프론트엔드 통신 허용
-app.use(express.json());                                        //JSON 형식의 데이터 받을 수 있게 설정
+app.use(cors());                                                // 프론트엔드 통신 허용
+app.use(express.json());                                        // JSON 형식의 데이터 받을 수 있게 설정
 
 // 라우트 연결
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));                 // URL경로별로 어떤 파일이 처리할지 연결
 app.use('/api/inventory', require('./routes/inventory'));
 app.use('/api/orders', require('./routes/orders'));
+// app.use('/api/pos', require('./routes/pos'));                  이 후 pos api 연동을 한다면 연결
 
 // 서버 시작
 app.listen(PORT, () => {
